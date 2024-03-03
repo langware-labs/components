0063ec8a81ac3a9735c4351e1f88c5f839aadf82
ran-langware <ran@langware.ai>
2024-03-03 16:20:59 +0200

    Add shell-script component - part 1 - adding changed files. Next commit will include all other files, with are copies of code-snippet with simple name replace.

---
+++ shell-script/docs/src/index.md
@@ -0,0 +1,53 @@
+---
+layout: page.11ty.cjs
+title: <shell-script> ⌲ Home
+---
+
+# &lt;shell-script>
+
+`<shell-script>` is an awesome element. It's a great introduction to building web components with LitElement, with nice documentation site as well.
+
+## Configure with JS
+
+<section class="columns">
+  <div>
+
+`<shell-script>` can be configured with JS.
+
+```html
+<script>
+  const pythonScript = document.createElement('shell-script');
+  pythonScript.data  =  {
+    "content": "import math\\nprint(math.sqrt(16))\\nprint(\"Hello, World!\")",
+    "type": "python"
+  };
+  document.body.appendChild(pythonScript);
+</script>          
+<script>
+  const shellScript = document.createElement('shell-script');
+  shellScript.data  =  {
+    "content": "ls", 
+    "type": "bash"
+  };
+  document.body.appendChild(shellScript);
+</script>   
+```
+
+  </div>
+</section>
+<script>
+  const pythonScript = document.createElement('shell-script');
+  pythonScript.data  =  {
+    "content": "import math\\nprint(math.sqrt(16))\\nprint(\"Hello, World!\")",
+    "type": "python"
+  };
+  document.body.appendChild(pythonScript);
+</script>          
+<script>
+  const shellScript = document.createElement('shell-script');
+  shellScript.data  =  {
+    "content": "ls", 
+    "type": "bash"
+  };
+  document.body.appendChild(shellScript);
+</script>   
---
+++ shell-script/src/shell-script.ts
@@ -0,0 +1,153 @@
+import {Extension} from '@codemirror/state';
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {EditorState} from '@codemirror/state';
+import {EditorView, keymap, highlightActiveLine} from '@codemirror/view';
+import {defaultKeymap} from '@codemirror/commands';
+import {StreamLanguage} from "@codemirror/language";
+import {shell as shellLang} from '@codemirror/legacy-modes/mode/shell';
+import {python as pythonLang} from '@codemirror/lang-python';
+import {oneDark} from '@codemirror/theme-one-dark';
+import {basicSetup} from 'codemirror';
+
+@customElement('shell-script')
+export class ShellScript extends LitElement {
+  @property({ type: Object }) data: { type: string, content: string } = { type: '', content: '' };
+  
+  private shellCode = '';
+  private shellType = '';
+  private editorParentNode?: HTMLElement;
+
+  static override styles = css`
+    :host {
+      display: block;
+      margin: auto auto 20px;
+      width: 80%;
+    }
+    .code-block {
+      position: relative;
+      border: 1px solid;    }
+    .run {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+    .result {
+      text-align: left;
+      margin: 0 -1px -1px;
+      border: 1px solid;
+      display: none;
+    }
+  `;
+
+  override firstUpdated() {
+    this.shellType = this.data.type;
+    this.shellCode =  this.data.content.replaceAll('\\n', '\n');
+    this.editorParentNode = this.shadowRoot?.querySelector(
+      '.code-editor-container'
+    ) as HTMLElement;
+    if (this.editorParentNode) {
+      this.initializeEditor(this.shellType);
+    }
+  }
+
+  initializeEditor(mode: string) {
+    const startState = EditorState.create({
+      doc: this.shellCode,
+      extensions: [
+        keymap.of(defaultKeymap),
+        basicSetup,
+        highlightActiveLine(),
+        oneDark,
+        this.getLanguageExtension(mode),
+        EditorView.updateListener.of((update) => {
+          if (update.docChanged) {
+            this.shellCode = update.state.doc.toString();
+          }
+        }),
+      ],
+    });
+
+    new EditorView({
+      state: startState,
+      parent: this.editorParentNode,
+    });
+  }
+
+  getLanguageExtension(mode: string) : Extension {
+    switch (mode) {
+      case 'bash':
+      case 'powershell':
+      case 'cmd':
+        return StreamLanguage.define(shellLang);
+      case 'python':
+        return pythonLang();
+      default:
+        return [];
+    }
+  }
+  
+  async runCode() {
+    const resultDom = this.shadowRoot?.querySelector(
+      'pre.result'
+    ) as HTMLPreElement;
+    if (resultDom) {
+      resultDom.style.display = 'block';
+      resultDom.textContent = await this.executeShell();
+    }
+  }
+
+  private async executeShell() {
+    let command = '';
+    switch (this.shellType) {
+      case 'bash':
+      case 'cmd':
+      case 'powershell': {
+        command = this.shellCode;
+        break;
+      }
+      case 'python': {
+        const escapedCode = this.shellCode.replaceAll('"', '\\"').replaceAll('\n', '; ');
+        command = `python -c "${escapedCode}"`;
+        break;
+      }
+      default:
+        return 'Invalid type';
+    }
+    try {
+      const response = await fetch('http://localhost:5000/execute', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ command }),
+      });
+      const data = await response.json();
+      
+      if (data.returnCode === 0 && (data.stderr === undefined || data.stderr === '')) {
+        return data.stdOut;
+      }
+      return 'returnCode: ' + data.returnCode +
+        (data.stdOut ? '\nstdout:\n' + data.stdOut : '') +
+        (data.stderr ? '\nstderr:\n' + data.stderr : '');
+    } catch (error) {
+      return 'error: ' + error;
+    }
+  }
+
+  override render() {
+    return html`
+      <div class="code-block">
+        <div class="code-editor-container"></div>
+        <button class="run" @click="${this.runCode}">Run</button>
+        <pre class="result"></pre>
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'shell-script': ShellScript;
+  }
+}
