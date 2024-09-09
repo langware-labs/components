ce6df88929faa948b4107d05677c5de2516cd71d
Roy Levi <roy@langware.ai>
2024-09-09 12:33:39 +0300

    update toolCommunicator

--- code-snippet/src/sdk/toolCommunicationService.ts
+++ code-snippet/src/sdk/toolCommunicationService.ts
@@ -1,30 +1,32 @@
-// Events for communication between tools - named opposite of the senders perspective
-const sendEventPrefix = 'receive-tool-message-';
-const listenEventPrefix = 'send-tool-message-';
-
 class ToolCommunicator {
   private callbacks: { [key: string]: (message: any) => void } = {};
+
   sendMessage(targetId: string, message: any) {
-    const toolEvent = new CustomEvent(sendEventPrefix + targetId, {
-      detail: { message: message }
+    const toolEvent = new CustomEvent(targetId, {
+      detail: { message: message },
     });
     window.dispatchEvent(toolEvent);
   }
 
-  listenForMessages(targetId: string, callback: (message: any) => void) {
+  listenForMessages(targetId: string, callback: (message: any) => void | Promise<void>) {
     if (this.callbacks[targetId]) {
-      throw new Error('Listener for targetId ' + targetId + ' already exists');
+      return;
     }
 
     const wrappedCallback = (event: any) => {
-      callback(event.detail.message)
+      // await if the callback returns a promise
+      const result = callback(event.detail.message);
+      if (result instanceof Promise) {
+        result.catch(console.error);
+      }
     };
     this.callbacks[targetId] = wrappedCallback;
-    window.addEventListener(listenEventPrefix + targetId, wrappedCallback);
+
+    window.addEventListener(targetId, wrappedCallback);
   }
 
   stopListening(targetId: string) {
-    window.removeEventListener(listenEventPrefix + targetId, this.callbacks[targetId]);
+    window.removeEventListener(targetId, this.callbacks[targetId]);
     delete this.callbacks[targetId];
   }
 }
