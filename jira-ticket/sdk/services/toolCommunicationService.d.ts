e000eac63a171e4e39997f9ed8ee4aaa93ab42ae
Roy Levi <roy@langware.ai>
2024-12-05 14:50:55 +0200

    update plugins schema

--- jira-ticket/sdk/services/toolCommunicationService.d.ts
+++
@@ -1,9 +0,0 @@
-declare class ToolCommunicator {
-    private callbacks;
-    sendMessage(targetId: string, message: any): void;
-    listenForMessages(targetId: string, callback: (message: any) => void): void;
-    stopListening(targetId: string): void;
-}
-export declare const toolCommunicator: ToolCommunicator;
-export default toolCommunicator;
-//# sourceMappingURL=toolCommunicationService.d.ts.map
